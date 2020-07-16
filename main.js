06ff57388fb1023a97891bd65ff58fc5e7f662de
Alberto Flores <alberto.floreso94@gmail.com>
2020-07-15 21:16:21 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var marca = 'Nissan'
+var modelo = 2019
+var presupuesto = 15000
+var transmicion = 'manual'
+
+for (var i = 0; i < autos.length; i++){
+    if (autos[i].make === marca && autos[i].year === modelo && (autos[i].price*.2) <= presupuesto && autos[i].transmision === transmicion){
+        console.log(autos[i])
+    }
+}
+
+SEGUIMOS MAÑANA JAJASIGE TRABAJKANDO ENLO TUIYO YO EN EL LANDING PAGE
+YA BAJE LOS ARCHIVOS
